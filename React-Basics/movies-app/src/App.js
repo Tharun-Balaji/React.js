98db3253fa5e5ea7011867f1586c33668a80b7d3
Tharun Balaji <tharunbalaji110@gmail.com>
2024-02-13 12:20:00 +0530

    Setup Folder For Movie App

---
+++ React-Basics/movies-app/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Home from './Components/Home';
+
+function App() {
+  return (
+    // <h1 class="text-3xl font-bold underline">
+    //   Hello world!
+    // </h1>
+
+    <div className="App">
+      <header className="App-header">
+        <h2>Movie App</h2>
+        <Home></Home>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ React-Basics/movies-app/src/Components/Home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import NavBar from "./NavBar";
+import Banner from "./Banner";
+import TrendingMovies from "./TrendingMovies";
+import Pagination from "./Pagination";
+
+function Home(){
+    return(
+        <>
+            <div>Home Page</div>
+            {/* // navbar */}
+            <NavBar></NavBar>
+            {/* // banner section */}
+            <Banner></Banner>
+            {/* // Trending Movies */}
+            <TrendingMovies></TrendingMovies>
+            {/* // pagination */}
+            <Pagination></Pagination>
+        </>
+    );
+};
+
+export default Home;
+
