3ca8e8f145408d200172682d898c25b4c075c319
Tharun Balaji <tharunbalaji110@gmail.com>
2024-03-13 12:00:42 +0530

    implemented search

---
+++ React-Basics/react-weather-app/src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'da91133d6emsh8c23cefb762ef4ep1fd891jsnf8755287ccfe',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+  
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = ""; // enter your key from openweather API
