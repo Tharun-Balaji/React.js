26b19e57beac2aab78f0f27c9b924ad112278b19
Tharun Balaji <tharunbalaji110@gmail.com>
2024-03-19 00:52:10 +0530

    Add Author to PostList

--- React-Projects/post-app/src/features/posts/PostsList.jsx
+++ React-Projects/post-app/src/features/posts/PostsList.jsx
@@ -3,19 +3,22 @@ import { useSelector  } from "react-redux";
 
 import React from 'react'
 import { selectAllPosts } from "./postsSlice";
+import PostAuthor from "./PostAuthor";
 
 
 function PostsList() {
     const posts = useSelector (selectAllPosts)
 
 
-    const renderedPosts = posts.map(post => (
-        <article key={post.id}>
-            <h3>{post.title}</h3>
-            <p>{post.content.substring(0, 100)}</p>
-            
-        </article>
-    ))
+    const renderedPosts = posts.map((post) => (
+      <article key={post.id}>
+        <h3>{post.title}</h3>
+        <p>{post.content.substring(0, 100)}</p>
+        <p className="postCredit">
+          <PostAuthor userId={post.userId} />
+        </p>
+      </article>
+    ));
 
     return (
         <section>
