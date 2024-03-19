deae7329b5f7d5c2ed4de53d6162e8cb12006934
Tharun Balaji <tharunbalaji110@gmail.com>
2024-03-19 13:03:18 +0530

    Added Recation Button and TimeStamp

--- React-Projects/post-app/src/features/posts/PostsList.jsx
+++ React-Projects/post-app/src/features/posts/PostsList.jsx
@@ -1,31 +1,36 @@
-import { useSelector  } from "react-redux";
+import { useSelector } from "react-redux";
 
-
-import React from 'react'
+import React from "react";
 import { selectAllPosts } from "./postsSlice";
 import PostAuthor from "./PostAuthor";
-
+import TimeAgo from "./TimeAgo";
+import ReactionButtons from "./ReactionButtons";
 
 function PostsList() {
-    const posts = useSelector (selectAllPosts)
+  const posts = useSelector(selectAllPosts);
 
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
 
-    const renderedPosts = posts.map((post) => (
-      <article key={post.id}>
-        <h3>{post.title}</h3>
-        <p>{post.content.substring(0, 100)}</p>
-        <p className="postCredit">
-          <PostAuthor userId={post.userId} />
-        </p>
-      </article>
-    ));
+  const renderedPosts = orderedPosts.map((post) => (
+    <article key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content.substring(0, 100)}</p>
+      <p className="postCredit">
+        <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
+      </p>
+      <ReactionButtons post={post} />
+    </article>
+  ));
 
-    return (
-        <section>
-            <h2>Posts</h2>
-            {renderedPosts}
-        </section>
-    )
+  return (
+    <section>
+      <h2>Posts</h2>
+      {renderedPosts}
+    </section>
+  );
 }
 
-export default PostsList
+export default PostsList;
