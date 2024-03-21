f8702cf8cfb74f4d64f4eaad37ac9a5899d59d84
Tharun Balaji <tharunbalaji110@gmail.com>
2024-03-22 00:03:53 +0530

    completed Hero Section

---
+++ React-Projects/portfolio-app/src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+
+import styles from "./Hero.module.css";
+
+export default function Hero() {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi I'm Tharun</h1>
+        <p className={styles.description}>
+        I'm a Aspiring software developer passionate about crafting elegant code, solving complex problems,
+                and continuously learning to innovate in the ever-evolving world of technology.
+        </p>
+        <a href="mailto:tharunbalaji110@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src="/assets/hero/MyPhoto.png"
+        alt="HeroImage of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  )
+}
+
--- React-Projects/portfolio-app/src/utils.js
+++
@@ -1,3 +0,0 @@
-export function getImageUrl(path){
-    return new URL(`/assets/${path}`, import.meta.url).href;
-}
