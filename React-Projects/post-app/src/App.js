2bef01ed849bfe9a6942ffb14ed1d6a651ecc0a7
Tharun Balaji <tharunbalaji110@gmail.com>
2024-03-21 08:56:32 +0530

    created react Portfolio app with create-react-app

--- React-Projects/post-app/src/App.js
+++ React-Projects/post-app/src/App.js
@@ -1,31 +1,30 @@
-
-import './App.css';
-import PostsList from './features/posts/PostsList';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import AddPostForm from './features/posts/AddPostForm';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import SinglePostPage from './features/posts/SinglePostPage';
-import Layout from './components/Layout';
-import EditPostForm from './features/posts/EditPostForm';
-
-
+import "./App.css";
+import PostsList from "./features/posts/PostsList";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import AddPostForm from "./features/posts/AddPostForm";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SinglePostPage from "./features/posts/SinglePostPage";
+import Layout from "./components/Layout";
+import EditPostForm from "./features/posts/EditPostForm";
+import { Navigate } from "react-router-dom";
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<PostsList />} />
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<PostsList />} />
 
-          <Route path="post">
-            <Route index element={<AddPostForm />} />
-            <Route path=":postId" element={<SinglePostPage />} />
-            <Route path="edit/:postId" element={<EditPostForm />} />
+            <Route path="post">
+              <Route index element={<AddPostForm />} />
+              <Route path=":postId" element={<SinglePostPage />} />
+              <Route path="edit/:postId" element={<EditPostForm />} />
+            </Route>
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Route>
-        </Route>
-      </Routes>
+        </Routes>
       </BrowserRouter>
     </Provider>
   );
