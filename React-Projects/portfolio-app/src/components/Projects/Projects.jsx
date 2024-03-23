4a569d3df46da2a54efe1f3bd0f5f2c51739d92a
Tharun Balaji <tharunbalaji110@gmail.com>
2024-03-23 18:18:56 +0530

    Completed Project Section, But still need to add all of my projects

---
+++ React-Projects/portfolio-app/src/components/Projects/Projects.jsx
@@ -0,0 +1,23 @@
+
+import projects from "../../data/projects.json"
+import ProjectCard from "./ProjectCard"
+import styles from "./Projects.module.css"
+
+export default function Projects() {
+  return (
+    <section className= {styles.container} >
+        <h2 className= {styles.title} >Projects</h2>
+        <div className= {styles.project} >
+        {
+            projects.map(
+                function(project,id){
+                    return(
+                        <ProjectCard key={id} project = {project} />
+                    )
+                }
+            )
+        }
+        </div>
+    </section>
+  )
+}
