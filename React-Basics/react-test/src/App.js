435b36549cb6a0156b58c6ca07e3fe36daf214b8
Tharun Balaji <tharunbalaji110@gmail.com>
2024-03-31 19:53:10 +0530

    Played some React Hooks

--- React-Basics/react-test/src/App.js
+++ React-Basics/react-test/src/App.js
@@ -1,27 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './Components/Login/Login';
+import React, { useReducer } from 'react';
+
+const initialState = { count: 0 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { count: state.count + 1 };
+    case 'DECREMENT':
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+}
+
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const decrement = () => {
+    dispatch({ type: 'DECREMENT' });
+    dispatch({ type: 'DECREMENT' });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Login/>
-      </header>
-     
-    </div>
+    <>
+      <div>Count: {state.count}</div>
+      <button onClick={() => dispatch({ type: 'INCREMENT' })}>Increment</button>
+      <button onClick={decrement}>Decrement Twice</button>
+    </>
   );
 }
 
