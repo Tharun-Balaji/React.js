404917c183ae334b359d6329181579ea83c13cc3
Tharun Balaji <tharunbalaji110@gmail.com>
2024-04-08 08:33:06 +0530

    created a debounce

--- React-Basics/react-test/src/App.js
+++ React-Basics/react-test/src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-import Login from './Components/Login/Login';
-import React, { useReducer } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import Login from './Components/Login/Login';
+import React, { useEffect, useReducer, useState } from 'react';
 
 const initialState = { count: 0 };
 
@@ -18,19 +18,38 @@ function reducer(state, action) {
 
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, setState] = useState("");
 
-  const decrement = () => {
-    dispatch({ type: 'DECREMENT' });
-    dispatch({ type: 'DECREMENT' });
-  };
+  // const decrement = () => {
+  //   dispatch({ type: 'DECREMENT' });
+  //   dispatch({ type: 'DECREMENT' });
+  // };
+
+  useEffect( () => {
+    const debounce = setTimeout(() => {
+      console.log(state)
+    }, 500);
+      console.log(debounce);
+    return () => {
+      clearTimeout(debounce);
+    };
+  }, [state] )
 
   return (
-    <>
-      <div>Count: {state.count}</div>
-      <button onClick={() => dispatch({ type: 'INCREMENT' })}>Increment</button>
-      <button onClick={decrement}>Decrement Twice</button>
-    </>
+    <div style={{
+      display : "flex",
+      flexDirection: "column",
+      alignItems : "center",
+      justifyContent : "center",
+      height : "100vh",
+      width : "100vw",
+    }} >
+      <input 
+       value={state}
+       onChange={(e) => setState(e.target.value)}
+      />
+      <p>{state}</p>
+    </div>
   );
 }
 
