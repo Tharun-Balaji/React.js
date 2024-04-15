023cad53e1e6f24b15aa86424cc534e28d2b3ddf
Tharun Balaji <tharunbalaji110@gmail.com>
2024-04-15 09:36:12 +0530

    Provided context to App Through Providers in Home Page

--- React-Projects/crypto-app/src/pages/Home.jsx
+++ React-Projects/crypto-app/src/pages/Home.jsx
@@ -3,18 +3,19 @@ import { Outlet } from 'react-router-dom'
 import Logo from '../Components/Logo'
 import Navigation from '../Components/Navigation'
 import { CryptoProvider } from '../context/CryptoContext'
+import { TrendingProvider } from '../context/TrendingContext'
 
 export default function Home() {
   return (
-    <CryptoProvider>
-    <main className='  w-full h-full flex flex-col first-letter: content-center items-center relative text-white font-nunito' >
-      <div className=' w-screen h-screen bg-gray-300 fixed -z-10' >
-
-      </div>
-      <Logo/>
-      <Navigation/>
-      <Outlet/>
-    </main>
-    </CryptoProvider>
-  )
+    <TrendingProvider>
+      <CryptoProvider>
+        <main className="  w-full h-full flex flex-col first-letter: content-center items-center relative text-white font-nunito">
+          <div className=" w-screen h-screen bg-gray-300 fixed -z-10"></div>
+          <Logo />
+          <Navigation />
+          <Outlet />
+        </main>
+      </CryptoProvider>
+    </TrendingProvider>
+  );
 }
