38906ffd5435fbc1e2b69f3a12732bc1590eb861
Tharun Balaji <tharunbalaji110@gmail.com>
2024-04-15 09:32:55 +0530

    created Trending Context

---
+++ React-Projects/crypto-app/src/context/TrendingContext.js
@@ -0,0 +1,40 @@
+import { createContext, useEffect, useState } from "react";
+
+
+
+const TrendingContext = createContext({});
+
+
+export function TrendingProvider({children}){
+    const [trendData, setTrendData] = useState();
+
+    async function getTrendData() {
+      try {
+        const data = await fetch(
+          "https://api.coingecko.com/api/v3/search/trending"
+        )
+          .then((res) => {
+            res.json();
+          })
+          .then((json) => json);
+
+        console.log(data);
+        setTrendData(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    function resetTrendingResults() {
+        getTrendData()
+    }
+
+    useEffect(() => {getTrendData()},[])
+
+    return (
+        <TrendingContext.Provider value={{trendData, resetTrendingResults}}>
+            {children}
+        </TrendingContext.Provider>
+    )
+
+}
