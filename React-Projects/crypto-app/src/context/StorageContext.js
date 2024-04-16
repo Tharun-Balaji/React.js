29dd5a3f323690568ee0d460517af232bdf48f0a
Tharun Balaji <tharunbalaji110@gmail.com>
2024-04-16 12:55:35 +0530

    Implemente Saved Pages

--- React-Projects/crypto-app/src/context/StorageContext.js
+++ React-Projects/crypto-app/src/context/StorageContext.js
@@ -1,41 +1,87 @@
-import { createContext, useLayoutEffect, useState } from "react";
+import {
+  createContext,
+  useContext,
+  useLayoutEffect,
+  useState,
+  useEffect,
+} from "react";
+import { CryptoContext } from "./CryptoContext";
 
 export const StorageContext = createContext({});
 
 export function StorageProvider({ children }) {
   const [allCoins, setAllCoins] = useState([]);
+  const [savedData, setSavedData] = useState();
+  const { currency, sortBy } = useContext(CryptoContext);
 
-  useLayoutEffect(() => {
-    // const isThere = JSON.parse(localStorage.getItem("coins")) || false;
+  console.log(currency)
+//   console.log(allCoins)
 
-    if (localStorage.getItem("coins")) {
-      // set the state with value from local storage
-      setAllCoins(JSON.parse(localStorage.getItem("coins")));
-    } else {
-      // set local storage with an Empty Array
-      localStorage.setItem("coins", JSON.stringify([]));
-    }
-  }, []);
+  const saveCoin = (coinId) => {
+    let oldCoins = JSON.parse(localStorage.getItem("coins"));
 
-  function saveCoin(coinId) {
-    const oldCoins = JSON.parse(localStorage.getItem("coins"));
     if (oldCoins.includes(coinId)) {
-      return;
+      return null;
     } else {
-      const newCoins = [...oldCoins, coinId];
-      setAllCoins(newCoins);
-      localStorage.setItem("coins", JSON.stringify(newCoins));
+      let newCoin = [...oldCoins, coinId];
+      setAllCoins(newCoin);
+      localStorage.setItem("coins", JSON.stringify(newCoin));
     }
-  }
+  };
 
-  function removeCoin(coinId) {
-    const oldCoins = JSON.parse(localStorage.getItem("coins"));
+  const removeCoin = (coinId) => {
+    let oldCoins = JSON.parse(localStorage.getItem("coins"));
 
-    const newCoin = oldCoins.filter((coin) => coin !== coinId);
+    let newCoin = oldCoins.filter((coin) => coin !== coinId);
 
     setAllCoins(newCoin);
     localStorage.setItem("coins", JSON.stringify(newCoin));
-  }
+  };
+
+  const getSavedData = async (totalCoins = allCoins) => {
+    console.log(totalCoins);
+    try {
+      const data = await fetch(
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${totalCoins.join(",")}&order=${sortBy}&sparkline=false&price_change_percentage=1h%2C24h%2C7d&x_cg_demo_api_key=CG-xPTDuU1xWf9V99UybnaCu79t`
+      )
+        .then((res) => res.json())
+        .then((json) => json);
+
+      // console.log(data);
+      setSavedData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const resetSavedResult = () => {
+    getSavedData();
+  };
+
+  useEffect(() => {
+    if (allCoins.length > 0) {
+      getSavedData(allCoins);
+    } else {
+      setSavedData();
+    }
+  }, [allCoins]);
+
+  useLayoutEffect(() => {
+    let isThere = JSON.parse(localStorage.getItem("coins")) || false;
+
+    if (!isThere) {
+      //set the localstorage with empty array
+      localStorage.setItem("coins", JSON.stringify([]));
+    } else {
+      //set the state with the current values from the local storage
+      let totalCoins = JSON.parse(localStorage.getItem("coins"));
+      setAllCoins(totalCoins);
+
+      if (totalCoins.length > 0) {
+        getSavedData(totalCoins);
+      }
+    }
+  }, []);
 
   return (
     <StorageContext.Provider
@@ -44,6 +90,8 @@ export function StorageProvider({ children }) {
         setAllCoins,
         saveCoin,
         removeCoin,
+        savedData,
+        resetSavedResult,
       }}
     >
       {children}
