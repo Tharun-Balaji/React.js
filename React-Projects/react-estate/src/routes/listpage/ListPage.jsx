3036ae67071615f19b0f33c53fef23affc0eef32
Tharun Balaji <tharunbalaji110@gmail.com>
2024-06-13 12:35:53 +0530

    Created list page layout

--- React-Projects/react-estate/src/routes/listpage/ListPage.jsx
+++ React-Projects/react-estate/src/routes/listpage/ListPage.jsx
@@ -1,7 +1,13 @@
-import "./ListPage.scss"
+import "./ListPage.scss";
+import { listData } from "../../lib/dummyData";
 
 export default function ListPage() {
   return (
-    <div>ListPage</div>
+    <div className="listPage" >
+      <div className="listContainer">
+        <div className="wrapper"></div>
+      </div>
+      <div className="mapContainer">Map</div>
+    </div>
   )
 }
