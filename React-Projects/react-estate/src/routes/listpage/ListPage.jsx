027027adcbb9ce87cee8d79ecee3b19c69b0aafe
Tharun Balaji <tharunbalaji110@gmail.com>
2024-06-13 14:36:43 +0530

    Created filter component

--- React-Projects/react-estate/src/routes/listpage/ListPage.jsx
+++ React-Projects/react-estate/src/routes/listpage/ListPage.jsx
@@ -1,11 +1,21 @@
 import "./ListPage.scss";
 import { listData } from "../../lib/dummyData";
+import Filter from './../../components/filter/Filter';
+import Card from "./../../components/card/Card";
 
 export default function ListPage() {
+  const data = listData
   return (
     <div className="listPage" >
       <div className="listContainer">
-        <div className="wrapper"></div>
+        <div className="wrapper">
+          <Filter />
+          {
+            data.map((item) => (
+              <Card key={item.id} item={item} />
+            )) 
+          }
+        </div>
       </div>
       <div className="mapContainer">Map</div>
     </div>
