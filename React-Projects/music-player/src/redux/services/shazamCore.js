1ace8849e600404126daa0b80ac15e37ec2c3233
Tharun Balaji <tharunbalaji110@gmail.com>
2024-06-30 16:48:32 +0530

    created shazamCore service

---
+++ React-Projects/music-player/src/redux/services/shazamCore.js
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core7.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', 'da91133d6emsh8c23cefb762ef4ep1fd891jsnf8755287ccfe');
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: '/charts/world' }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
