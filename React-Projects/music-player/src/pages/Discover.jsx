4f0f64d7343daf1f2ca13d76a8b180163a5b9e86
Tharun Balaji <tharunbalaji110@gmail.com>
2024-06-30 20:44:46 +0530

    fetched songs

--- React-Projects/music-player/src/pages/Discover.jsx
+++ React-Projects/music-player/src/pages/Discover.jsx
@@ -1,10 +1,18 @@
 
-import { Error, Loader, SongCard } from "../components";
-import { genres } from "../assets/constants";
+import { Error, Loader, SongCard } from '../components';
+import { genres } from '../assets/constants';
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 function Discover() {
-  console.log(genres);
-  const generaTitle = "Pop";
+  const generaTitle = 'Pop';
+  const { data, isFetching, error } = useGetTopChartsQuery();
+
+  if (isFetching) return <Loader title="Loading songs..." />;
+
+  if (error) return <Error />;
+
+  // console.log(data);
+
   return (
     <div className="flex flex-col">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
@@ -28,7 +36,7 @@ function Discover() {
       </div>
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {
-          [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (<SongCard key={song.key} song={song} i={i} />))
+          data?.data?.map((song, i) => (<SongCard key={song.id} song={song} i={i} />))
         }
       </div>
     </div>
