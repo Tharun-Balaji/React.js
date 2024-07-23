8192a960e214a676dca62fb12a599029c4932eca
Tharun Balaji <tharunbalaji110@gmail.com>
2024-07-23 18:31:36 +0530

    Added AddReactionMutation to Reaction Button Components

--- React-Projects/post-app/src/features/posts/ReactionButtons.jsx
+++ React-Projects/post-app/src/features/posts/ReactionButtons.jsx
@@ -1,5 +1,5 @@
 import { useDispatch } from "react-redux";
-import { reactionAdded } from "./postsSlice";
+import { reactionAdded, useAddReactionMutation } from "./postsSlice";
 
 const reactionEmoji = {
     thumbsUp: '👍',
@@ -11,17 +11,20 @@ const reactionEmoji = {
 
 
 
-export default function ReactionButtons({post}) {
-    const dispatch = useDispatch();
+export default function ReactionButtons({ post }) {
+    
+    const [addReaction] = useAddReactionMutation();
+
     const reactionButtons = Object.entries(reactionEmoji).map(([name, emoji]) => {
         return (
             <button
                 key={name}
                 type="button"
                 className="reactionButton"
-                onClick={() =>
-                    dispatch(reactionAdded({ postId: post.id, reaction: name }))
-                }
+                onClick={() =>{
+                    const newValue = post.reactions[name] + 1;
+                    addReaction({ postId: post.id, reactions: { ...post.reactions, [name]: newValue } })
+                }}
             >
                 {emoji} {post.reactions[name]}
             </button>
