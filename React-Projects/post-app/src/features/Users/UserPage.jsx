dcda9bc804804102f4d623af5d7910286615e5ee
Tharun Balaji <tharunbalaji110@gmail.com>
2024-07-23 18:20:44 +0530

    Added GetPostsByUserIdQuery to UserPage

--- React-Projects/post-app/src/features/Users/UserPage.jsx
+++ React-Projects/post-app/src/features/Users/UserPage.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
 import { selectUserById } from "./usersSlice";
-import { selectAllPosts, selectPostsByUser } from "../posts/postsSlice";
+import { useGetPostsByUserIdQuery } from "../posts/postsSlice";
 
   
 
@@ -10,17 +10,33 @@ export default function UserPage() {
   const { userId } = useParams();
   const user = useSelector((state) => selectUserById(state, Number(userId)));
 
-  const postsForUsers = useSelector((state) => selectPostsByUser(state, Number(userId)));
+  const {
+    data: postsForUsers,
+    isLoading,
+    isSuccess,
+    isError,
+    error
+  } = useGetPostsByUserIdQuery(userId);
 
-  const postTitles = postsForUsers.map((post) => <li key={post.id} >
-    <Link to={`/post/${post.id}`}>{post.title}</Link>
-  </li>);
+  let content;
+  if (isLoading) {
+		content = <p>Loading...</p>;
+  } else if (isSuccess) {
+		const { ids, entities } = postsForUsers;
+		content = ids.map((id) => (
+			<li key={id}>
+				<Link to={`/post/${id}`}>{entities[id].title}</Link>
+			</li>
+		));
+  } else if (isError) {
+		content = <p>{error}</p>;
+  }
 
   return (
 		<section className="user-page">
 			<h2>{user.name}</h2>
 
-			<ul>{postTitles}</ul>
+			<ul>{content}</ul>
 		</section>
   );
 }
