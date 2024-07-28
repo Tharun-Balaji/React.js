0ddfa2bfcbd4e701a6d269415c6a5a73da810974
Tharun Balaji <tharunbalaji110@gmail.com>
2024-07-28 14:00:19 +0530

    Styled the Single Post Page

--- React-Projects/post-app/src/features/posts/SinglePostPage.jsx
+++ React-Projects/post-app/src/features/posts/SinglePostPage.jsx
@@ -1,36 +1,45 @@
-import {useSelector} from 'react-redux';
-import { selectPostById } from './postsSlice';
-import PostAuthor from "./PostAuthor"
-import TimeAgo from "./TimeAgo"
-import ReactionButtons from "./ReactionButtons"
-import { useParams , Link} from 'react-router-dom';
-
+import { useSelector } from "react-redux";
+import { selectPostById } from "./postsSlice";
+import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
+import ReactionButtons from "./ReactionButtons";
+import { useParams, Link } from "react-router-dom";
 
 export default function SinglePostPage() {
-    const { postId } = useParams()
+	const { postId } = useParams();
 
-    console.log(postId)
+	console.log(postId);
 
-    const post = useSelector((state) => selectPostById(state, Number(postId)))
+	const post = useSelector((state) => selectPostById(state, Number(postId)));
 
-    if (!post) {
-        return (
-            <section>
-                <h2>Post not found!</h2>
-            </section>
-        )
-    }
+	if (!post) {
+		return (
+			<section>
+				<h2>Post not found!</h2>
+			</section>
+		);
+	}
 
-    return (
-		<article>
-			<h2>{post.title}</h2>
-			<p>{post.body}</p>
-			<p className="postCredit">
-				<PostAuthor userId={post.userId} />
-				<TimeAgo timestamp={post.date} />
-			</p>
-			<ReactionButtons post={post} />
-			<Link className="link" to={`/post/edit/${post.id}`}>Edit Post</Link>
-		</article>
+	return (
+		<div className="h-screen flex items-center justify-center">
+			<article className="max-w-3xl bg-white p-8 rounded-md shadow-md">
+				<h2 className="text-xl font-bold mb-2" >{post.title}</h2>
+				<p className="text-gray-700">{post.body}</p>
+				<p className="flex items-center justify-between mt-2">
+					<PostAuthor userId={post.userId} />
+					<TimeAgo
+						timestamp={post.date}
+						className="text-sm text-gray-500"
+					/>
+				</p>
+				<ReactionButtons post={post} />
+				<Link
+					to={`/post/edit/${post.id}`}
+					className="text-blue-500 hover:underline px-2 py-1 rounded-md font-medium"
+				>
+					Edit Post
+				</Link>
+			</article>
+		</div>
 	);
 }
