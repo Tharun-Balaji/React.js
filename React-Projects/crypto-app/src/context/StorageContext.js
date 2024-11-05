69b85947ae5d1351c8a73040628bb5e807687cbe
Tharun Balaji <tharunbalaji110@gmail.com>
2024-11-05 12:11:06 +0530

    removed logs

--- React-Projects/crypto-app/src/context/StorageContext.js
+++ React-Projects/crypto-app/src/context/StorageContext.js
@@ -39,7 +39,7 @@ export function StorageProvider({ children }) {
   };
 
   const getSavedData = async (totalCoins = allCoins) => {
-    console.log(totalCoins);
+    // console.log(totalCoins);
     try {
       const data = await fetch(
         `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&ids=${totalCoins.join(",")}&order=${sortBy}&sparkline=false&price_change_percentage=1h%2C24h%2C7d&x_cg_demo_api_key=CG-xPTDuU1xWf9V99UybnaCu79t`
