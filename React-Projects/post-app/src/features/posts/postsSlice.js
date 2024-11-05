64df2a4408afeb30d85b9f8d2fba36a0531db345
Tharun Balaji <tharunbalaji110@gmail.com>
2024-11-05 10:53:30 +0530

    added comments to redux postSlice

--- React-Projects/post-app/src/features/posts/postsSlice.js
+++ React-Projects/post-app/src/features/posts/postsSlice.js
@@ -5,16 +5,26 @@ import {
 import { sub } from "date-fns";
 import { apiSlice } from "../api/apiSlice";
 
-
-
+// This adapter is used to normalize the posts data and provide
+// functions to work with it. The sortComparer function is used
+// to sort the posts by date in descending order.
 const postsAdapter = createEntityAdapter({
   sortComparer: (a, b) => b.date.localeCompare(a.date)
 });
 
+// This is the initial state of the posts slice. It is an empty
+// object that will be populated with the posts data when the
+// getPosts endpoint is called.
 const initialState = postsAdapter.getInitialState();
 
 export const extendedApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
+    // This endpoint is used to get all the posts. It returns
+    // an object with the posts data and the total number of
+    // posts. The transformResponse function is used to add
+    // a default date and reactions to each post if they are
+    // missing. The providesTags function is used to set the
+    // tags for the posts so that they can be cached.
     getPosts: builder.query({
       query: () => '/posts',
       transformResponse: responseData => {
@@ -37,6 +47,12 @@ export const extendedApiSlice = apiSlice.injectEndpoints({
         ...result.ids.map(id => ({ type: 'Post', id }))
       ]
     }),
+    // This endpoint is used to get all the posts for a specific user.
+    // It returns an object with the posts data and the total number of
+    // posts. The transformResponse function is used to add a default
+    // date and reactions to each post if they are missing. The providesTags
+    // function is used to set the tags for the posts so that they can be
+    // cached.
     getPostsByUserId: builder.query({
       query: id => `/posts/?userId=${id}`,
       transformResponse: responseData => {
@@ -58,6 +74,9 @@ export const extendedApiSlice = apiSlice.injectEndpoints({
         ...result.ids.map(id => ({ type: 'Post', id }))
       ]
     }),
+    // This endpoint is used to add a new post. It returns an object
+    // with the new post data. The invalidatesTags function is used
+    // to set the tags for the posts so that they can be cached.
     addNewPost: builder.mutation({
       query: initialPost => ({
         url: '/posts',
@@ -79,6 +98,9 @@ export const extendedApiSlice = apiSlice.injectEndpoints({
         { type: 'Post', id: "LIST" }
       ]
     }),
+    // This endpoint is used to update a post. It returns an object
+    // with the updated post data. The invalidatesTags function is used
+    // to set the tags for the posts so that they can be cached.
     updatePost: builder.mutation({
       query: initialPost => ({
         url: `/posts/${initialPost.id}`,
@@ -92,6 +114,9 @@ export const extendedApiSlice = apiSlice.injectEndpoints({
         { type: 'Post', id: arg.id }
       ]
     }),
+    // This endpoint is used to delete a post. It returns an object
+    // with the deleted post data. The invalidatesTags function is used
+    // to set the tags for the posts so that they can be cached.
     deletePost: builder.mutation({
       query: ({ id }) => ({
         url: `/posts/${id}`,
@@ -102,6 +127,9 @@ export const extendedApiSlice = apiSlice.injectEndpoints({
         { type: 'Post', id: arg.id }
       ]
     }),
+    // This endpoint is used to add a reaction to a post. It returns
+    // an object with the updated post data. The onQueryStarted function
+    // is used to update the cache when the reaction is added.
     addReaction: builder.mutation({
       query: ({ postId, reactions }) => ({
         url: `posts/${postId}`,
@@ -140,11 +168,11 @@ export const {
 } = extendedApiSlice;
 
 // returns the query result object
-// This code block defines a selector function named selectPostsResult that extracts the result of the getPosts endpoint from extendedApiSlice.
+// This selector function is used to get the result of the getPosts endpoint.
 export const selectPostsResult = extendedApiSlice.endpoints.getPosts.select()
 
-
 // Creates memoized selector
+// This selector function is used to get the posts data from the cache.
 const selectPostsData = createSelector(
   selectPostsResult,
   postsResult => postsResult.data // normalized state object with ids & entities
@@ -155,3 +183,4 @@ export const {
   selectById: selectPostById,
   selectIds: selectPostIds
 } = postsAdapter.getSelectors((state) => selectPostsData(state) ?? initialState);
+
