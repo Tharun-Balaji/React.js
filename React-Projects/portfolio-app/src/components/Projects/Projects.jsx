1d9fcd47996c1b085821f6a681bf485d98dcbd1a
Tharun Balaji <tharunbalaji110@gmail.com>
2024-11-12 11:09:36 +0530

    fixed styles

--- React-Projects/portfolio-app/src/components/Projects/Projects.jsx
+++ React-Projects/portfolio-app/src/components/Projects/Projects.jsx
@@ -7,7 +7,7 @@ export default function Projects() {
   return (
     <section className= {styles.container} id = "projects" >
         <h2 className= {styles.title} >Projects</h2>
-        <div className= {styles.project} >
+        <div className= {styles.projects} >
         {
             projects.map(
                 function(project,id){
